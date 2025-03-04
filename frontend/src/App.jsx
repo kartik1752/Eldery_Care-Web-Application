026589e02be76817498a8b03e7b01d09263300d1
Kartik <kartjh7@gmail.com>
2025-03-04 06:25:24 +0530

    Fix routing issue and add redirect from / to /signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { SignUp } from "./Components/SignUp";
@@ -13,6 +13,7 @@ function App() {
     <Router>
       <ToastContainer position="top-right" autoClose={3000} />
       <Routes>
+        <Route path="/" element={<Navigate to="/signup" />} /> {/* Redirect to signup */}
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin-dashboard" element={<AdminDashBoard />} />
